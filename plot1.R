3f595a7389aca409cb46b7a5f1b609335fc5525e
Henning <henninglee2013@gmail.com>
2016-04-18 23:26:34 +1000

    Plot1 finish

---
+++ plot1.R
@@ -0,0 +1,27 @@
+
+## set parth
+setwd("C:/Users/Bruce/Dropbox/R/Exploratory Data Analysis")
+
+## Electric power consumption data(epc)
+epc <- read.table("household_power_consumption.txt", sep = ";",quote ="", header = TRUE)
+
+## conver factor format to date format for column Date in epc
+epc$Date <- as.Date(epc$Date,"%d/%m/%Y")
+## conver factor format to time format for column Time in epc
+epc$Time <- strptime(epc$Time, "%H:%M:%S")
+
+## filter data between 2007-02-01 and 2007-02-02        
+subepc <- subset(epc, Date %in% as.Date(c("2007-02-01","2007-02-02")))
+head(subepc)
+
+
+## plot graph
+
+global_active_power <- as.numeric(levels(subepc$Global_active_power))[subepc$Global_active_power]
+str(global_active_power)
+
+hist(global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
+title(main = "Global Active Power")
+
+dev.copy(png, "plot1.png")
+dev.off()
